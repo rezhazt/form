4fb0e41f9cb03c543dbc808b674f4751373286ac
rezhazt <rezha.print@gmail.com>
2021-01-13 11:10:14 +0700

    Update menyimpan-daftar.php
    update code

--- menyimpan-daftar.php
+++ menyimpan-daftar.php
@@ -16,10 +16,11 @@ $test4=$_POST["test4"];
 $test5=$_POST["test5"];
 $test6=$_POST["test6"];
 $test7=$_POST["test7"];
+$test8=$_POST["test8"];
 
 //Query input menginput data kedalam tabel barang
-  $sql="insert into tabel_visit (idCard,fullname,wa,email,visitDate,tujuan,test1,test2,test3,test4,test5,test6,test7) values
-		('$idCard','$fullname','$wa','$email','$visitDate','$tujuan','$test1','$test2','$test3','$test4','$test5','$test6','$test7')";
+  $sql="insert into tabel_visit (idCard,fullname,wa,email,visitDate,tujuan,test1,test2,test3,test4,test5,test6,test7,test8) values
+		('$idCard','$fullname','$wa','$email','$visitDate','$tujuan','$test1','$test2','$test3','$test4','$test5','$test6','$test7','$test8')";
 
 //Mengeksekusi/menjalankan query diatas	
   $hasil=mysqli_query($kon,$sql);
